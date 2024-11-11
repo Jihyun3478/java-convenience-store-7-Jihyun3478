c180087acca01fbcf18856acbcdd138c1a992d7c
Jihyun3478 <leejihyun3478@gmail.com>
2024-11-11 21:29:42 +0900

    fix : PromotionType 및 OutputMessage 반영

---
+++ src/main/java/store/constant/PromotionType.java
@@ -0,0 +1,18 @@
+package store.constant;
+
+public enum PromotionType {
+	CARBONATE_DRINK("탄산2+1"),
+	MD_RECOMMEND_PRODUCT("MD추천상품"),
+	FLASH_DISCOUNT("반짝할인"),
+	;
+
+	private final String promotionName;
+
+	PromotionType(String promotionName) {
+		this.promotionName = promotionName;
+	}
+
+	public String getPromotionName() {
+		return promotionName;
+	}
+}
